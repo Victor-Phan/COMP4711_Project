9739016ea0c89ec7e1a0ffdc5b54dd63cc8fbbfb
Henry Ho <henrychho89@gmail.com>
2020-03-05 22:14:38 -0800

    Added empty files as templates

---
+++ server/index.js
@@ -0,0 +1,29 @@
+  
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+const expressHbs = require("express-handlebars");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "client/views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs"
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", "client/views");
+
+app.get("/", (req, res) => res.send("Hello world"))
+
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}`));
