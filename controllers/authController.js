c068b28bd74f7df5adb997a569f0343fa13e84bf
Henry Ho <henrychho89@gmail.com>
2020-03-07 15:37:35 -0800

    Added bcrypt for password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,38 @@
+const bcrypt = require("bcrypt");
 const { user } = require("../models");
+
+const saltRounds = process.env.SALT_ROUNDS || 10;
+
+const encryptPassword = async pw => {
+  try {
+    const encrypted = await new Promise ((res, rej) => {
+      bcrypt.hash(pw, saltRounds).then(res).catch(rej)
+    })
+    return encrypted
+  } catch (e) {
+    throw e;
+  }
+};
+
+const isPasswordValid = async (input, pw) => {
+  try {
+    const result = await new Promise((res, rej) => {
+      bcrypt.compare(input, pw).then(res).catch(rej);
+    })
+    return result;
+  } catch (e) {
+    throw e;
+  }
+};
 
 exports.signup = async (req, res, next) => {
   try {
     const newUser = req.body;
+    newUser.password = await encryptPassword(newUser.password);
+
     await user.insertUser(newUser);
     req.session.user = { email: newUser.email };
-    res.send(newUser);
+    res.send(req.session.user);
   } catch (e) {
     next({ ...e, message: "Error signing user" });
   }
@@ -18,10 +45,10 @@ exports.signin = async (req, res, next) => {
     const validUser = data[0];
 
     if (data.length == 0) throw new Error("No such user");
-    if (password !== validUser.password) throw new Error("Invalid password");
+    if (!await isPasswordValid(password, validUser.password)) throw new Error("Invalid password");
 
     req.session.user = { email: validUser.email, id: validUser.id };
-    res.send(validUser);
+    res.send(req.session.user);
   } catch (e) {
     next(e);
   }
