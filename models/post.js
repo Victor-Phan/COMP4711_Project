d58989a430dd9b6365e14332d4ed2aba14d753c9
Henry Ho <henrychho89@gmail.com>
2020-03-29 14:39:32 -0700

    Added sql statement to get all post properties

--- models/post.js
+++ models/post.js
@@ -1,8 +1,8 @@
-const { promisifyQuery } = require('./helperFunctions.js');
+const { promisifyQuery } = require("./helperFunctions.js");
 
 function insertPost(e) {
   let { user_id, type, subject, message } = e;
-  let sql = `INSERT INTO post (user_id, type, subject, message) 
+  let sql = `INSERT INTO post (user_id, type_id, subject, message) 
                VALUES ('${user_id}', '${type}', '${subject}', '${message}')`;
   return promisifyQuery(sql);
 }
@@ -29,6 +29,23 @@ function getPostsByType(type) {
 
 function getPostsBySubject(filter) {
   let sql = `SELECT * FROM post WHERE subject LIKE '%${filter}%'`;
+  return promisifyQuery(sql);
+}
+
+function getPostWithAllProperties(id) {
+  let sql = `SELECT post.id, type_id, message, user.first_name, user.last_name, user.image_url, COUNT(postcomment.id) AS replies
+  FROM post 
+  LEFT JOIN (
+  SELECT id, first_name, last_name, image_url 
+  FROM user 
+  ) user
+  ON post.user_id = user.id 
+  LEFT JOIN (
+  SELECT id, post_id
+  FROM postcomment
+  ) postcomment
+  ON post.id = postcomment.post_id
+  WHERE post.id = ${post_id}`;
   return promisifyQuery(sql);
 }
 
