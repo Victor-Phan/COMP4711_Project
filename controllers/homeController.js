cfa595aa3553f4ec3a8a5990f026033b72d15375
Victor Phan <victor.phann@gmail.com>
2020-04-03 23:45:17 -0700

    added next and prev post buttons

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,11 @@
 const { posttypeModel, postModel, postcommentModel } = require("../models");
 
-exports.getHomePage = async (req, res, next) => {
+exports.getHomePage = async(req, res, next) => {
     const postTypes = await posttypeModel.getPostTypes();
     const posts = await postModel.getAllPostsDESC();
-  
+
     //maybe get each post comment for each post here..
     //FE does not accomodate this yet.
-  
-    return res.render("home", { navbarCSS: true, postTypes, posts });
+
+    return res.render("home", { navbarCSS: true, homeCSS: true, postTypes, posts });
 }
