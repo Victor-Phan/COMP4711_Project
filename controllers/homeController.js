9dfde55e4913c3ded47de64725bfc5f4c45fa908
Victor Phan <victor.phann@gmail.com>
2020-04-04 16:08:44 -0700

    added post comments to home controller

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,15 +4,24 @@ exports.getHomePage = async(req, res, next) => {
     const postTypes = await posttypeModel.getPostTypes();
     const posts = await postModel.getAllPostsDESC();
 
-    //maybe get each post comment for each post here..
-    //FE does not accomodate this yet.
+    //Get all comments within each post
+    const processedPosts = posts.map(async post => {
+        const postWithReplies = await postcommentModel.getPostComments(post.id);
 
-    return res.render("home", {
-        navbarCSS: true,
-        homeCSS: true,
-        passwordCheck: true,
-        homePageJS: true,
-        postTypes,
-        posts
+        return Object.assign({}, post, { comments: postWithReplies });
     });
+
+    Promise.all(processedPosts)
+        .then(completed =>
+            res.render("home", {
+                navbarCSS: true,
+                homeCSS: true,
+                passwordCheck: true,
+                homePageJS: true,
+                postTypes,
+                posts: completed
+            }))
+        .catch(err => {
+            throw err;
+        });
 }
