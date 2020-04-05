2fedfe28e0977188740408a4eafafb2259d8ea06
Victor Phan <victor.phann@gmail.com>
2020-04-04 17:03:40 -0700

    added grabbing user details for home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,27 +1,37 @@
-const { posttypeModel, postModel, postcommentModel } = require("../models");
+const { posttypeModel, postModel, postcommentModel, userModel } = require("../models");
 
 exports.getHomePage = async(req, res, next) => {
-    const postTypes = await posttypeModel.getPostTypes();
-    const posts = await postModel.getAllPostsDESC();
+    try {
+        const { id: user_id } = req.session.user;
+        const postTypes = await posttypeModel.getPostTypes();
+        const posts = await postModel.getAllPostsDESC();
+        const userDetails = await userModel.getUserDetails(user_id);
+        if (userDetails.length == 0) {
+            throw new Error(`User not found: ${user_id}`);
+        }
 
-    //Get all comments within each post
-    const processedPosts = posts.map(async post => {
-        const postWithReplies = await postcommentModel.getPostComments(post.id);
+        //Get all comments within each post
+        const processedPosts = posts.map(async post => {
+            const postWithReplies = await postcommentModel.getPostComments(post.id);
 
-        return Object.assign({}, post, { comments: postWithReplies });
-    });
-
-    Promise.all(processedPosts)
-        .then(completed =>
-            res.render("home", {
-                navbarCSS: true,
-                homeCSS: true,
-                passwordCheck: true,
-                homePageJS: true,
-                postTypes,
-                posts: completed
-            }))
-        .catch(err => {
-            throw err;
+            return Object.assign({}, post, { comments: postWithReplies });
         });
+
+        Promise.all(processedPosts)
+            .then(completed =>
+                res.render("home", {
+                    navbarCSS: true,
+                    homeCSS: true,
+                    passwordCheck: true,
+                    homePageJS: true,
+                    postTypes,
+                    posts: completed,
+                    userDetails: userDetails[0]
+                }))
+            .catch(err => {
+                throw err;
+            });
+    } catch (err) {
+        next(err);
+    }
 }
--- models/user.js
+++ models/user.js
@@ -1,79 +1,96 @@
 const { promisifyQuery } = require("./helperFunctions.js");
 
-const baseSQL = `SELECT id, first_name, last_name, email, image_url, about, country, DATE_FORMAT(dob, "%Y-%m-%d") as dob, profilelike.count as profileLikes
-                FROM user 
-                LEFT JOIN (
-                SELECT COUNT(*) AS count, user_profile_liked
-                FROM profilelike
-                ) profilelike
-                ON user.id = profilelike.user_profile_liked`;
+const baseSQL = `SELECT id, first_name, last_name, email, image_url, about, country, 
+                        DATE_FORMAT(dob, "%Y-%m-%d") as dob,
+                        IFNULL(profilelike.count,0) as profileLikes,
+                        IFNULL(message.count,0) as messageCount,
+                        IFNULL(post.count,0) as postCount
+                        FROM user 
+                        LEFT JOIN (
+                          SELECT COUNT(*) AS count, user_profile_liked
+                          FROM profilelike
+                          GROUP BY user_profile_liked
+                        ) profilelike
+                        ON user.id = profilelike.user_profile_liked
+				                LEFT JOIN (
+                          SELECT COUNT(*) AS count, recipient_id
+                          FROM message
+                          GROUP BY recipient_id
+                          ) message
+                          ON user.id = message.recipient_id
+				                LEFT JOIN (
+                          SELECT COUNT(*) AS count, user_id
+                          FROM post
+                          GROUP BY post.user_id
+                        ) post
+                        ON user.id = post.user_id`;
 
 function insertUser(e) {
-  let {
-    first_name,
-    last_name,
-    email,
-    password,
-    image_url,
-    about,
-    country,
-    dob
-  } = e;
-  let sql = `INSERT INTO user (first_name, last_name, email, password, image_url, about, country, dob) 
+    let {
+        first_name,
+        last_name,
+        email,
+        password,
+        image_url,
+        about,
+        country,
+        dob
+    } = e;
+    let sql = `INSERT INTO user (first_name, last_name, email, password, image_url, about, country, dob) 
                VALUES ('${first_name}', '${last_name}', '${email}', '${password}', '${image_url}', '${about}', '${country}', '${dob}')`;
-  return promisifyQuery(sql);
+    return promisifyQuery(sql);
 }
 
 function updateUser(e) {
-  //Cannot update password or email,
-  let { id, first_name, last_name, image_url, about, country, dob } = e;
-  let sql = `UPDATE user SET first_name = '${first_name}', 
+    //Cannot update password or email,
+    let { id, first_name, last_name, image_url, about, country, dob } = e;
+    let sql = `UPDATE user SET first_name = '${first_name}', 
                                 last_name = '${last_name}', 
                                 image_url = '${image_url}', 
                                 about = '${about}', 
                                 country = '${country}', 
                                 dob = '${dob}' 
                                 WHERE (id = '${id}')`;
-  return promisifyQuery(sql);
+    return promisifyQuery(sql);
 }
 
 function getUser(id) {
-  let sql = `SELECT * FROM user WHERE id = ${id}`;
-  return promisifyQuery(sql);
+    let sql = `SELECT * FROM user WHERE id = ${id}`;
+    return promisifyQuery(sql);
 }
 
 function getUserDetails(id) {
-  let sql = `${baseSQL} WHERE id = '${id}'`;
-  return promisifyQuery(sql);
+    let sql = `${baseSQL} WHERE id = '${id}'`;
+    return promisifyQuery(sql);
 }
 
 function getAllUsersDetail() {
-  let sql = baseSQL;
-  return promisifyQuery(sql);
+    let sql = baseSQL;
+    return promisifyQuery(sql);
 }
 
 function getUserByEmail(email) {
-  let sql = `SELECT * FROM user where email = '${email}'`;
-  return promisifyQuery(sql);
+    let sql = `SELECT * FROM user where email = '${email}'`;
+    return promisifyQuery(sql);
 }
 
 function getAllUserIdsWithExistingMessages(user_id) {
-  let sql = `SELECT DISTINCT user.id 
+    let sql = `SELECT DISTINCT user.id 
             FROM user user
             INNER JOIN message message ON
                 user.id =  message.recipient_id OR
                 user.id =  message.sender_id
             WHERE 
                 message.recipient_id = '${user_id}' OR message.sender_id = '${user_id}'`;
-  return promisifyQuery(sql);
+    return promisifyQuery(sql);
 }
 
 module.exports = {
-  insertUser,
-  updateUser,
-  getUser,
-  getUserByEmail,
-  getUserDetails,
-  getAllUsersDetail,
-  getAllUserIdsWithExistingMessages
+    insertUser,
+    updateUser,
+    getUser,
+    getUserByEmail,
+    getUserDetails,
+    getAllUsersDetail,
+    getAllUserIdsWithExistingMessages
 };
