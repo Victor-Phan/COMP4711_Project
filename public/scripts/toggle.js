7a92f0006ee27da91ecf88868a0a9fadde115f51
TomasQuat <tquatito@gmail.com>
2020-04-06 09:54:43 -0700

    Improved toggle functionality
    Toggle function setup to work on the home and posts page. Adapts to the different layouts being used.
    Added onclick handlers to the corresponding elements in the partials

--- public/scripts/toggle.js
+++ public/scripts/toggle.js
@@ -1,8 +1,19 @@
 function toggle_replies(e) {
 
-    let replyBox = e.parentNode.nextElementSibling;
+    console.log(e);
+
+    let replyBox;
+
+    if (e.classList.contains("search-results--item-replies-count")) {
+        replyBox = e.parentNode.nextElementSibling;
+    } else if (e.classList.contains("div-replyNum")) {
+        replyBox = e.nextElementSibling;
+    }
+
+    console.log("Reply Box:");
+    console.log(replyBox);
 
     replyBox.style.display === "none"
         ? (replyBox.style.display = "block")
         : (replyBox.style.display = "none");
-}   
+}
