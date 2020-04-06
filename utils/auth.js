1550ce24028953f94eb2b90f908e168dfa8d2a16
Henry Ho <henrychho89@gmail.com>
2020-04-05 18:56:14 -0700

    Added favicon

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,5 @@
-// Chrome attempts to call this route, which causes an error. We should provide a favicon to prevent this
-const STATIC_ROUTES = ['/favicon.ico'];
-
 const checkSignin = (req, res, next) => {
-  if (req.session.user || STATIC_ROUTES.includes(req.url)) {
+  if (req.session.user) {
     next();
   } else {
     next(new Error('User not signed in'));
