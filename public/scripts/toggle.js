76970d4cef720066cda4eb6fc23260d70793c0ec
TomasQuat <tquatito@gmail.com>
2020-04-06 09:04:46 -0700

    Toggle function

--- public/scripts/toggle.js
+++ public/scripts/toggle.js
@@ -2,10 +2,6 @@ function toggle_replies(e) {
 
     let replyBox = e.parentNode.nextElementSibling;
 
-    console.log(replyBox.style.display);
-
-    //replyBox.toggle()
-
     replyBox.style.display === "none"
         ? (replyBox.style.display = "block")
         : (replyBox.style.display = "none");
