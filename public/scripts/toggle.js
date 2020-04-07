0117347159a70d86a23831b443f473a9b9b4a1ff
TomasQuat <tquatito@gmail.com>
2020-04-07 03:14:46 -0700

    Updated toggle function added onclick handler in post_replies hbs

--- public/scripts/toggle.js
+++ public/scripts/toggle.js
@@ -7,7 +7,7 @@ function toggle_replies(e) {
     if (e.classList.contains("search-results--item-replies-count")) {
         replyBox = e.parentNode.nextElementSibling;
     } else if (e.classList.contains("div-replyNum")) {
-        replyBox = e.nextElementSibling;
+        replyBox = e.parentNode.parentNode.parentNode.nextElementSibling;
     }
 
     console.log("Reply Box:");
